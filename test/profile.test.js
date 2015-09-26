14a3ddf63389e1cd98bd7a8ac5b2bc3674e5f0cf
Nelson <contact.nelsonic@gmail.com>
2015-09-26 13:17:57 +0100

    parser finds all languages and levels of proficiency. closes https://github.com/nelsonic/linkedin-public-profile-parser/issues/4

--- test/profile.test.js
+++ test/profile.test.js
@@ -6,7 +6,6 @@ var profile = require('../lib/profile');
 test('Parse Simon\'s Public Profile Page', function(t) {
   var file = __dirname + '/fixtures/simonlab.html'
   fs.readFile(file, function(err, html){
-    // console.log(err, html);
     var $ = cheerio.load(html);
     var url = 'https://uk.linkedin.com/in/simonlab';
     profile($,url, function(err, data){
@@ -16,7 +15,7 @@ test('Parse Simon\'s Public Profile Page', function(t) {
       t.ok(data.location === 'London, Greater London, United Kingdom', 'Location: '+data.location)
       t.ok(data.current === 'Founders & Coders C.I.C', 'Current Work: '+data.current);
       t.ok(data.picture.indexOf('.jpg') > -1, 'Profile Picture: '+data.picture);
-      console.log(data);
+      t.ok(data.languages[2].indexOf('Espagnol') > -1, 'Simon knows '+ data.languages[2]);
       t.end();
     })
   })
@@ -30,10 +29,6 @@ test('Parse Abdi\'s Public Profile Page', function(t) {
     profile($,url, function(err, data){
       t.ok(data.connections > 36, 'Abdi has: '+data.connections + ' connections');
       t.ok(data.summary.indexOf('teacher') > -1, 'Summary contains "teacher"');
-      // t.ok(data.fullname === 'Simon Labondance', 'Fullname: '+data.fullname);
-      // t.ok(data.location === 'London, Greater London, United Kingdom', 'Location: '+data.location)
-      // t.ok(data.current === 'Founders & Coders C.I.C', 'Current Work: '+data.current);
-      // t.ok(data.picture.indexOf('.jpg') > -1, 'Profile Picture: '+data.picture);
       console.log(data);
       t.end();
     })
@@ -48,34 +43,23 @@ test('Parse Elon\'s Public Profile Page', function(t) {
     var url = 'https://www.linkedin.com/in/emusk';
     profile($,url, function(err, data){
       t.ok(data.connections === 500, 'Elon has: '+data.connections + ' connections');
-      // t.ok(data.summary.indexOf('teacher') > -1, 'Summary contains "teacher"');
       t.ok(data.fullname === 'Elon Musk', 'Fullname: '+data.fullname);
-      // t.ok(data.location === 'London, Greater London, United Kingdom', 'Location: '+data.location)
-      // t.ok(data.current === 'Founders & Coders C.I.C', 'Current Work: '+data.current);
-      // t.ok(data.picture.indexOf('.jpg') > -1, 'Profile Picture: '+data.picture);
-      // console.log(data);
       t.end();
     })
   })
 })
 
-test.only('Parse Nelson\'s Public Profile Page', function(t) {
+test('Parse Nelson\'s Public Profile Page', function(t) {
   var file = __dirname + '/fixtures/nelsonic.html'
   fs.readFile(file, function(err, html){
-    // console.log(err, html);
     var $ = cheerio.load(html);
     var url = 'https://www.linkedin.com/in/nelsonic';
     profile($,url, function(err, data){
       t.ok(data.connections === 500, 'Nelson has: '+data.connections + ' connections');
-      // t.ok(data.summary.indexOf('teacher') > -1, 'Summary contains "teacher"');
-      // t.ok(data.fullname === 'Simon Labondance', 'Fullname: '+data.fullname);
-      // t.ok(data.location === 'London, Greater London, United Kingdom', 'Location: '+data.location)
-      // t.ok(data.current === 'Founders & Coders C.I.C', 'Current Work: '+data.current);
       t.ok(data.skills.length > 42, 'Skills: '+data.skills.length);
       t.ok(data.skills.indexOf('Node.js') > -1, 'Nelson knows Node');
       t.ok(data.languages.length > 5, 'Nelson knows '+ data.languages[5]);
       t.ok(data.languages[4].indexOf('Afrikaans') > -1, 'Nelson knows '+ data.languages[4]);
-      // console.log(data.languages);
       t.end();
     })
   })
