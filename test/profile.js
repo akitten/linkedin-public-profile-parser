34e647a6e33f0aa60307680edbf673589fd4c270
Nelson <contact.nelsonic@gmail.com>
2015-09-26 05:58:34 +0100

    adds response debug to fix travis build https://travis-ci.org/nelsonic/linkedin-public-profile-parser/builds/82278147

--- test/profile.js
+++ test/profile.js
@@ -1,10 +1,10 @@
 var test     = require('tape');
 var lp = require('../lib/index');
 
-// test('Force url error by not setting a url', function(t){
-// 	var url;
-// 	lp(url, function(err, data){
-//     t.ok(err === 404, 'Instant 404 Error when url not set');
-// 		t.end();
-// 	})
-// })
+test('Parse Simon\'s Public Profile Page', function(t){
+  var url = 'https://uk.linkedin.com/in/simonlab';
+	lp(url, function(err, data){
+    t.ok(err === null, 'Instant 404 Error when url not set');
+		t.end();
+	})
+})
