b3d208de73b1f084623890a232bb179f21e07779
Nelson <contact.nelsonic@gmail.com>
2015-09-26 10:35:59 +0100

    parse number of connections on profile card. see: https://github.com/nelsonic/linkedin-public-profile-parser/issues/2

---
+++ test/profile.test.js
@@ -0,0 +1,18 @@
+var test    = require('tape');
+var fs      = require('fs');
+var cheerio = require('cheerio');
+var profile = require('../lib/profile');
+
+test('Parse Simon\'s Public Profile Page', function(t) {
+  var file = __dirname + '/fixtures/simonlab.html'
+  fs.readFile(file, function(err, html){
+    // console.log(err, html);
+    var $ = cheerio.load(html);
+    var url = 'https://uk.linkedin.com/in/simonlab';
+    profile($,url, function(err, data){
+      t.ok(err === null, 'No Errors when parsing Simon\'s public profile page.');
+      t.ok(data.connections > 68, 'Simon has: '+data.connections + ' connections');
+      t.end();
+    })
+  })
+})
