3948303d3863f1052f91b7dedf030febe85c8a9e
SimonLab <simon.labondance@gmail.com>
2015-10-08 14:10:00 +0100

    return a data with the url when an error exists

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,7 @@ test('Force url error by not setting a url', function(t){
 	var url;
 	lp(url, function(err, data){
     t.ok(err === 404, 'Instant 404 Error when url not set');
+	t.ok(data.url === undefined, 'Url is undefined when it is not set');
 		t.end();
 	})
 })
@@ -13,6 +14,7 @@ test('Force 404 error by supplying a mal-formed url', function(t){
 	var url = 'https://uk.linkedin.com/in/simonlabond';
 	lp(url, function(err, data){
     t.ok(err === 404, 'Instant 404 Error when url not set');
+	t.ok(data.url === 'https://uk.linkedin.com/in/simonlabond', 'Return the mal-formed url');
 		t.end();
 	})
 })
