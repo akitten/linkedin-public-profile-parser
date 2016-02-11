1aca44997d4736eec77044439a311400cb25915b
SimonLab <simon.labondance@gmail.com>
2016-02-11 11:56:04 +0000

    update test for https://github.com/akitten/linkedin-public-profile-parser/pull/35#issuecomment-182821478

--- test/index.test.js
+++ test/index.test.js
@@ -14,8 +14,7 @@ test('Force url error by not setting a url', function(t){
 test('Force 404 error by supplying a mal-formed url', function(t){
 	var url = 'https://uk.linkedin.com/in/simonlabondwrong';
 	lp(url, function(err, data){
-		console.log('error 4040', err);
-    t.ok(err === 404, 'Instant 404 Error when url not set');
+    t.ok(parseInt(err) === 404, 'Instant 404 Error when url not set');
 	  t.ok(data.url === 'https://uk.linkedin.com/in/simonlabondwrong', 'Return the mal-formed url');
 		t.end();
 	})
@@ -49,5 +48,3 @@ test('Successfully Parse Elon\'s Public LinkedIn Profile Page ', function(t){
 		t.end();
 	})
 })
-
-// https://www.linkedin.com/in/emusk
